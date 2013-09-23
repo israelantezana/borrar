9ef16a101e740e1519963cf739047c6120090938
Israel Antezana R <israelantezana@gmail.com>
2013-09-23 18:29:43 -0400

    commit inicial

---
+++ README.md
@@ -0,0 +1,9 @@
+[![Build Status](https://travis-ci.org/israelantezana/FizzBuzz.png?branch=master)](https://travis-ci.org/israelantezana/FizzBuzz)
+
+FizzBuzz
+========
+
+Peque–o proyecto de ejemplo para el desarrollo del kata FizzBuzz usando TDD y SCM con las siguientes herramientas:
+- Maven
+- Git, Github
+- Travis
---
+++ src/main/java/fizzbuzz/FizzBuzzServlet.java
@@ -0,0 +1,17 @@
+package fizzbuzz;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class FizzBuzzServlet extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String hasta = request.getParameter("hasta");
+		response.getWriter().println("Se recibi— el parametro: " + hasta);
+	}
+}
