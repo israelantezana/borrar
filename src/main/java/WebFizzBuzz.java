108aebfeaa7469ce374a2f5cf150007c6d9542d1
Israel Antezana R <israelantezana@gmail.com>
2013-09-23 19:16:53 -0400

    Cambios para intentar HRK

--- src/main/java/WebFizzBuzz.java
+++ src/main/java/WebFizzBuzz.java
@@ -9,7 +9,7 @@ import org.eclipse.jetty.servlet.*;
 public class WebFizzBuzz extends HttpServlet {
 
 	public static void main(String[] args) throws Exception{
-		Server server = new Server(Integer.valueOf(System.getenv("PORT")));
+		Server server = new Server();//(Integer.valueOf(System.getenv("PORT")));
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
         context.setContextPath("/");
         server.setHandler(context);
